2efdd2538f5c3d5242a5d4d0404d954d82e0792a
Isley Griffith <isley@realt.co>
2023-10-04 17:24:02 -0700

    Fix text alignment on mobile and smaller device screens

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -76,7 +76,7 @@ export default function Resume() {
   return (
     <div className="mx-0 p-8 lg:pt-[0em] lg:pb-[8em] md:pb-[12em] mb-[8em] md:mt-[0em] sm:mt-[4em]">
       <div className="grid w-full grid-cols-1 mx-2 sm:gap-8 xxs:gap-8 xxs:grid-cols-1 md:grid-cols-1 md:pt-24 sm:pt-12 xxs:pt-24 mobile:pt-32">
-        <h1 className="text-3xl font-bold lg:mx-24 lg:text-left md:text-left sm:text-left xxs:text-left mobile:text-center lg:mb-0 md:mb-0 sm:mb-0 xxs:mb-0 mobile:mb-8">
+        <h1 className="text-3xl font-bold lg:mx-24 lg:text-left md:text-center sm:text-center xxs:text-center mobile:text-center lg:mb-0 md:mb-0 sm:mb-0 xxs:mb-0 mobile:mb-8">
           Experience
         </h1>
         <ul
@@ -143,9 +143,11 @@ export default function Resume() {
             </li>
           ))}
         </ul>
-        <div className="lg:mx-24">
-          <div className="flex items-center mr-12 space-x-8">
-            <h1 className="mt-8 text-3xl font-bold">Skills</h1>
+        <div className="mt-8 lg:mx-24">
+          <div className="flex lg:justify-start md:justify-center sm:justify-center xxs:justify-center mobile:justify-center">
+            <h1 className="text-3xl font-bold">
+              Skills
+            </h1>
             {/* <div className="flex items-center flex-grow mt-8 space-x-4">
               <div className="flex w-full h-0 border-t border-dashed border-primary" />
               <div className="w-4 h-4 ml-2 bg-orange-400 border border-black rounded-full"></div>
