0bbb28f7cb411b80881ef4e6667efece42b053a5
Isley Griffith <isley@realt.co>
2023-10-20 13:50:46 -0700

    Add assets

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="mx-0 p-8 lg:pt-[0em] lg:pb-[8em] md:pb-[12em] mb-[8em] md:mt-[0em] sm:mt-[4em]">
+      <div className="grid w-full grid-cols-1 mx-2 sm:gap-8 xxs:gap-8 xxs:grid-cols-1 md:grid-cols-1 md:pt-24 sm:pt-12 xxs:pt-24 mobile:pt-32">
+        <h1 className="text-3xl">Projects</h1>
+        
+
+      </div>
+    </div>
+  );
 }
